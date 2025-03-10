1bdd0f2c77647a1e262303c6b057c9a5c7612493
chatlabsextension <info@chatlabs.se>
2025-03-10 23:14:33 +0100

    Update extensions.js

--- extensions.js
+++ extensions.js
@@ -4,7 +4,7 @@ export const FormExtension = {
   name: "FormExtension",
   type: "response",
   match: ({ trace }) =>
-    trace.type === "ext_form" || trace.payload.name === "ext_form",
+    trace.type === "ext_form" || trace.payload?.name === "ext_form",
   render: ({ trace, element }) => {
     const disableFooterInputs = (isDisabled) => {
       const chatDiv = document.getElementById("voiceflow-chat");
@@ -31,16 +31,13 @@ export const FormExtension = {
           font-size: 0.8em;
           color: #888;
         }
-        input[type="text"], input[type="email"], input[type="tel"] {
+        input[type="text"], input[type="email"] {
           width: 100%;
           border: none;
           border-bottom: 0.5px solid rgba(0, 0, 0, 0.1);
           background: transparent;
           margin: 5px 0;
           outline: none;
-        }
-        .phone {
-          width: 150px;
         }
         .invalid {
           border-color: red;
@@ -62,9 +59,6 @@ export const FormExtension = {
       <label for="email">Email</label>
       <input type="email" class="email" name="email" required pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,}$" title="Invalid email address"><br><br>
 
-      <label for="phone">Phone Number</label>
-      <input type="tel" class="phone" name="phone" required pattern="\\d+" title="Invalid phone number, please enter only numbers"><br><br>
-
       <input type="submit" class="submit" value="Submit">
     `;
 
@@ -73,16 +67,13 @@ export const FormExtension = {
 
       const name = formContainer.querySelector(".name");
       const email = formContainer.querySelector(".email");
-      const phone = formContainer.querySelector(".phone");
 
       if (
         !name.checkValidity() ||
-        !email.checkValidity() ||
-        !phone.checkValidity()
+        !email.checkValidity()
       ) {
         name.classList.add("invalid");
         email.classList.add("invalid");
-        phone.classList.add("invalid");
         return;
       }
 
@@ -91,7 +82,7 @@ export const FormExtension = {
 
       window.voiceflow.chat.interact({
         type: "complete",
-        payload: { name: name.value, email: email.value, phone: phone.value },
+        payload: { name: name.value, email: email.value },
       });
     });
 
